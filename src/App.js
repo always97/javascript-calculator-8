b9a221883681451b1f5ea5d2f45090dbf924d390
JunWoo Park <97427744+always97@users.noreply.github.com>
2025-10-20 23:17:17 +0900

    feat: 문자열 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async getUserNumber() {
+    const userInput = await MissionUtils.Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요. \n"
+    );
+  }
+
+  async run() {
+    await this.getUserNumber();
+  }
 }
 
 export default App;
